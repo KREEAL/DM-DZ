3b9f29340ab4b6aacbdaa781b5a0ce0636f0470d
Цветков Киллриал <kiryatsvetkov@mail.ru>
2021-12-13 17:11:25 +0300

    Added bulean

--- DM1/Program.cs
+++ DM1/Program.cs
@@ -36,14 +36,45 @@ namespace DM1
         {
             Stack<int> l = new Stack<int>();//стек для перестановок
             SortedSet<int> a = new SortedSet<int>();
-            StreamWriter sw1 = new StreamWriter("res1.txt");//файл для перестановок
+
 
             n = 5;
+
+            /* перестановки */
+            StreamWriter sw1 = new StreamWriter("perestanovki.txt");//файл для перестановок
+
             bool[] used = new bool[n];
             for (int i = 0; i < n; i++)
                 a.Add(i);
             Perestanovki(0,used,l,sw1);
             sw1.Close();
+
+            /* булеан  */
+            //сделано через двоичные последовательности
+            StreamWriter sw2 = new StreamWriter("bulean.txt");
+
+            List<int> buleanL = new List<int>(a);// перевел в лист для удобства работы с [i]
+            int count = (int)(Math.Pow(2, n) - 1);
+            var sets = new string[count];
+            for (int n = 1; n <= count; n++)
+            {
+                var s = "";
+
+                var tmp = n;
+                for (var i = 0; tmp > 0; i++)
+                {
+                    int reminder;
+                    tmp = Math.DivRem(tmp, 2, out reminder);
+                    if (reminder > 0)
+                        s += buleanL[i] + ",";
+                }
+                s = s.Substring(0, s.Length - 1);
+                sets[n - 1] = s;
+            }
+            Array.Sort(sets);
+            sw2.WriteLine(string.Join("\n", sets));
+            sw2.Close();
+
         }
     }
 }
