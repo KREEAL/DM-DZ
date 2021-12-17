d52855b8f236fc06283f6aacbd2ccab9139372f6
Цветков Киллриал <kiryatsvetkov@mail.ru>
2021-12-17 23:22:26 +0300

    Added dm1_4

---
+++ Dm1_4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Dm1_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr1 = new StreamReader("graph1.txt");
+            StreamReader sr2 = new StreamReader("graph2.txt");
+            string gr1 = sr1.ReadToEnd();
+            string gr2 = sr2.ReadToEnd();
+
+            if (gr1 == gr2) Console.WriteLine("Изоморфны");
+            else
+                Console.WriteLine("Не изоморфны");
+
+
+        }
+    }
+}
