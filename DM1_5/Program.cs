cba40d3e4ae7f58770da0b490c18b963fc6e0e2a
Цветков Киллриал <kiryatsvetkov@mail.ru>
2021-12-21 19:29:28 +0300

    Изменения лабы 5

--- DM1_5/Program.cs
+++ DM1_5/Program.cs
@@ -6,7 +6,7 @@ namespace DM1_5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Здравствуйте, Арсений Сергеевич");
+            Console.WriteLine("До свидания, школа!");
         }
     }
 }
