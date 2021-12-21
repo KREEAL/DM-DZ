c3542d6745c22b5b670bf5501394398ef7d6cf45
Цветков Киллриал <kiryatsvetkov@mail.ru>
2021-12-21 19:28:22 +0300

    Добавлена Лаба 5

---
+++ DM1_5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DM1_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Здравствуйте, Арсений Сергеевич");
+        }
+    }
+}
